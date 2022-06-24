ffaf1598f300ae5638bd685c54629f0b19b2aba8
abdoulkassoum <98455222+abdoulkassoum@users.noreply.github.com>
2022-06-24 23:29:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//initié minute
+setInterval(setClock, 1000)
+//variables
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+//fonct recup date
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+
+  
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+//fonction de rotation element
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
